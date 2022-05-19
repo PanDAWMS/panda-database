e381be171188bba216007c0a8ad5944d7b8963cf
tmaeno <tmaeno@bnl.gov>
2022-05-19 13:48:23 +0200

    hba.conf

--- initdb/panda_db_init.sh
+++ initdb/panda_db_init.sh
@@ -3,8 +3,8 @@ set -e
 
 DIR=/docker-entrypoint-initdb.d/sqls
 
-PANDA_PASSWORD=${PANDA_PASSWORD:-password}
-sed 's/${PANDA_PASSWORD}/'"${PANDA_PASSWORD}"'/g' ${DIR}/init_database.sql > /tmp/init_database.sql
+PANDA_DB_PASSWORD=${PANDA_DB_PASSWORD:-password}
+sed 's/${PANDA_DB_PASSWORD}/'"${PANDA_DB_PASSWORD}"'/g' ${DIR}/init_database.sql > /tmp/init_database.sql
 psql -U postgres -v ON_ERROR_STOP=1 -f /tmp/init_database.sql
 
 for COMP in PANDA PANDAMETA PANDAARCH PANDABIGMON DEFT PARTITION
