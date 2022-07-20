cc2caa5b9e50b11952a207cdf5bb08911151f726
tmaeno <tmaeno@bnl.gov>
2022-07-20 11:22:39 +0200

    nfilesmissing in jedi_datasets

--- initdb/sqls/init_step.sql
+++ initdb/sqls/init_step.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE panda_db;
-CREATE USER panda PASSWORD '${PANDA_DB_PASSWORD}';
+SELECT 'CREATE DATABASE panda_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'panda_db')\gexec
+SELECT E'CREATE USER panda PASSWORD \'${PANDA_DB_PASSWORD}\'' WHERE NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'panda')\gexec
 ALTER ROLE panda SET search_path = doma_panda,public;
-CREATE EXTENSION pg_cron;
+CREATE EXTENSION IF NOT EXISTS pg_cron;
 GRANT USAGE ON SCHEMA cron TO panda;
 \c panda_db;
-CREATE SCHEMA partman;
-CREATE EXTENSION pg_partman SCHEMA partman;
+CREATE SCHEMA IF NOT EXISTS partman;
+CREATE EXTENSION IF NOT EXISTS pg_partman SCHEMA partman;
