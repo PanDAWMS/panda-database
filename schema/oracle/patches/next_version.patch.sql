9fc78b59a9efe45fc4df2d1db19feb4aba5f7b3a
Edward Karavakis <edward.karavakis@cern.ch>
2023-06-28 15:11:11 +0200

    Corrected DB schema

--- schema/oracle/patches/next_version.patch.sql
+++ schema/oracle/patches/next_version.patch.sql
@@ -3,8 +3,8 @@
 ALTER TABLE "DOMA_PANDA"."JEDI_EVENTS" ADD ("ERROR_DIAG" VARCHAR2(500 BYTE));
 
 
-UPDATE "ATLAS_PANDA"."PANDADB_VERSION" SET major=0, minor=0, patch=15 where component='PANDABIGMON';
-UPDATE "ATLAS_PANDA"."PANDADB_VERSION" SET major=0, minor=0, patch=15 where component='JEDI';
-UPDATE "ATLAS_PANDA"."PANDADB_VERSION" SET major=0, minor=0, patch=15 where component='SERVER';
+UPDATE "DOMA_PANDA"."PANDADB_VERSION" SET major=0, minor=0, patch=15 where component='PANDABIGMON';
+UPDATE "DOMA_PANDA"."PANDADB_VERSION" SET major=0, minor=0, patch=15 where component='JEDI';
+UPDATE "DOMA_PANDA"."PANDADB_VERSION" SET major=0, minor=0, patch=15 where component='SERVER';
 
 COMMIT;
