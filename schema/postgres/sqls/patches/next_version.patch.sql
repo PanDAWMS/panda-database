524c03f4c92f0a82105422d098090add89654a97
Edward Karavakis <edward.karavakis@cern.ch>
2023-07-07 09:51:29 +0200

    0.0.15 patch for postgres.

--- schema/postgres/sqls/patches/next_version.patch.sql
+++ schema/postgres/sqls/patches/next_version.patch.sql
@@ -14,11 +14,4 @@ retention_keep_table = false
 WHERE parent_table = 'doma_panda.pandalog'
 ;
 
-ALTER TABLE doma_panda.jedi_events
-    error_diag varchar(500)
-;
-
-
-UPDATE doma_panda.pandadb_version set major=0, minor=0, patch=15 WHERE component = 'SERVER';
-UPDATE doma_panda.pandadb_version set major=0, minor=0, patch=15 WHERE component = 'JEDI';
 COMMIT;
