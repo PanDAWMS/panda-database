56d7f4e7fee1a56b1e9d5a16071ae7e08f7cde22
Edward Karavakis <edward.karavakis@cern.ch>
2023-09-06 14:02:32 +0200

    Added the update_realmodficiationtime trigger

---
+++ schema/postgres/sqls/pg_PANDA_TRIGGER.sql
@@ -0,0 +1,24 @@
+SET client_encoding TO 'UTF8';
+
+SET search_path = doma_panda,public;
+
+\set ON_ERROR_STOP ON
+
+SET check_function_bodies = false;
+
+DROP TRIGGER IF EXISTS update_realmodificationtime ON jedi_tasks CASCADE;
+-- Trigger to set JEDI_TASKS.REALMODIFICATIONTIME to current UTC timestamp
+CREATE OR REPLACE FUNCTION update_realmodificationtime_trg() RETURNS trigger AS $BODY$
+BEGIN
+    NEW.realmodificationtime := CURRENT_TIMESTAMP AT TIME ZONE 'UTC';
+RETURN NEW;
+END
+$BODY$
+ LANGUAGE 'plpgsql';
+
+ALTER FUNCTION update_realmodificationtime_trg() OWNER TO panda;
+
+CREATE TRIGGER update_realmodificationtime
+	BEFORE INSERT ON jedi_tasks FOR EACH ROW
+	EXECUTE PROCEDURE update_realmodificationtime_trg();
+
