b74063fd777b1243b1c93a799d3b05ce7e75bba6
Edward Karavakis <edward.karavakis@cern.ch>
2024-07-12 15:38:25 +0200

    Correct the patch files when running without pre-setting the path

--- schema/postgres/sqls/patches/0.0.17.patch.sql
+++ schema/postgres/sqls/patches/0.0.17.patch.sql
@@ -8,6 +8,6 @@ ALTER TABLE doma_pandaarch.jobsarchived ADD COLUMN outputfiletype varchar(32);
 
 ALTER TABLE doma_panda.jedi_process_lock ALTER COLUMN lockedby TYPE varchar(56);
 -- Update versions
-UPDATE pandadb_version SET major=0, minor=0, patch=17 where component='JEDI';
-UPDATE pandadb_version SET major=0, minor=0, patch=17 where component='SERVER';
+UPDATE doma_panda.pandadb_version SET major=0, minor=0, patch=17 where component='JEDI';
+UPDATE doma_panda.pandadb_version SET major=0, minor=0, patch=17 where component='SERVER';
 COMMIT;
