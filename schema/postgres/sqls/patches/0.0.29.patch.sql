1c51d42859825462025cd0e5556b36bc142803e0
Edward Karavakis <edward.karavakis@cern.ch>
2025-07-28 20:12:38 +0200

    Correct the update script for the 0.0.29 version

--- schema/postgres/sqls/patches/0.0.29.patch.sql
+++ schema/postgres/sqls/patches/0.0.29.patch.sql
@@ -1,6 +1,13 @@
 -- patch to be used to upgrade from version 0.0.28
 CREATE INDEX wn_metrics_timestamp_idx ON doma_panda.worker_node_metrics ("timestamp");
 
-UPDATE "ATLAS_PANDA"."PANDADB_VERSION" SET major=0, minor=0, patch=29 where component ='JEDI';
-UPDATE "ATLAS_PANDA"."PANDADB_VERSION" SET major=0, minor=0, patch=29 where component ='SERVER';
+-- Update schema version
+UPDATE doma_panda.pandadb_version
+SET major = 0, minor = 0, patch = 29
+WHERE component = 'JEDI';
+
+UPDATE doma_panda.pandadb_version
+SET major = 0, minor = 0, patch = 29
+WHERE component = 'SERVER';
+
 commit;
