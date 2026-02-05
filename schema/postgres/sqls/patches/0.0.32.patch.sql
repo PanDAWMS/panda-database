ee7d969ff3cb862067681febff79227b379a17db
Edward Karavakis <edward.karavakis@cern.ch>
2026-02-05 15:04:29 +0100

    Increase psql schema version

--- schema/postgres/sqls/patches/0.0.32.patch.sql
+++ schema/postgres/sqls/patches/0.0.32.patch.sql
@@ -4,3 +4,9 @@ ALTER TABLE doma_panda.jedi_tasks
 
 COMMENT ON COLUMN doma_panda.jedi_tasks.actiontime
   IS 'Timestamp when a periodic action is executed on the task';
+
+-- Update schema version
+UPDATE doma_panda.pandadb_version
+SET major = 0, minor = 0, patch = 32
+WHERE component = 'PanDA';
+commit;
